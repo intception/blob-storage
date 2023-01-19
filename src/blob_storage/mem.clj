a0bde1052d7e9ff1ef61535744b99c46b1960a6d
ichramm <jramirez.uy@gmail.com>
2023-01-19 17:39:16 -0300

    Fix in-memory storage

--- src/blob_storage/mem.clj
+++ src/blob_storage/mem.clj
@@ -1,9 +1,8 @@
 (ns blob-storage.mem
   (:gen-class)
   (:require [blob-storage.api :as api :refer [BlobStorage]]
-            [blob-storage.coerce :refer [coerce-blob]]
-            [blob-storage.postgres.schema :as schema]))
-
+            [blob-storage.coerce :refer [coerce-blob blob->bytes]]
+            [clojure.java.io :as io]))
 
 (defrecord Mem [config]
   BlobStorage
@@ -15,26 +14,52 @@
     (api/init-schema! service))
 
   (store! [service blob]
-    (let [id (str (java.util.UUID/randomUUID))]
-      (swap! config #(assoc-in % [:blobs id]
-                               {:id id
-                                :updated-at (java.util.Date.)
-                                :size (alength blob)
-                                :blob (coerce-blob blob)}))
+    (let [id (str (java.util.UUID/randomUUID))
+          [blob-size input-stream] (coerce-blob blob)]
+      (swap! config
+             #(assoc-in %
+                        [:blobs id]
+                        {:id id
+                         :created_at (java.util.Date.)
+                         :updated_at (java.util.Date.)
+                         :size blob-size
+                         :blob (blob->bytes input-stream blob-size)}))
+      id))
+
+  (store! [service blob {:keys [id tag] :as data}]
+    (let [id (or id (str (java.util.UUID/randomUUID)))
+          [blob-size input-stream] (coerce-blob blob)]
+      (swap! config
+             #(assoc-in %
+                        [:blobs id]
+                        {:id id
+                         :created_at (java.util.Date.)
+                         :updated_at (java.util.Date.)
+                         :size blob-size
+                         :tag tag
+                         :blob (blob->bytes input-stream blob-size)}))
       id))
 
   (update! [service id blob]
-    (swap! config #(assoc-in % [:blobs id]
-                             {:id id
-                              :updated-at (java.util.Date.)
-                              :size (alength blob)
-                              :blob (coerce-blob blob)})))
+    (let [[blob-size input-stream] (coerce-blob blob)]
+      (swap! config
+             #(update-in %
+                         [:blobs id]
+                         merge
+                         {:updated_at (java.util.Date.)
+                          :size blob-size
+                          :blob (blob->bytes input-stream blob-size)}))))
 
   (del! [service id]
-    (swap! config #(assoc-in % [:blobs id] nil)))
+    (swap! config #(update % :blobs dissoc id)))
 
   (blob [service id]
-    (get (:blobs @config) id)))
+    (when-let [record (get-in @config [:blobs id])]
+      (update record :blob io/input-stream)))
+
+  (blob-metadata [_ id]
+    (when-let [record (get-in @config [:blobs id])]
+      (dissoc record :blob))))
 
 (defn make
   []
---
+++ test/blob_storage/mem_test.clj
@@ -0,0 +1,36 @@
+(ns blob-storage.mem-test
+  (:require [blob-storage.api :as b]
+            [blob-storage.coerce :as bc]
+            [blob-storage.mem :as mem]
+            [blob-storage.test-common :refer :all])
+  (:use [clojure.test]))
+
+(def service (mem/make))
+
+(defn init-schema-fixture [f]
+  (try
+    (b/drop-schema! service)
+    (catch Exception e))
+  (b/init-schema! service)
+  (f))
+
+(use-fixtures :each init-schema-fixture)
+
+(deftest store-blob
+  (test-store-blob service))
+
+(deftest store-blob-with-id
+  (test-store-blob-with-id service))
+
+(deftest store-blob-with-tag
+  (test-store-blob-with-tag service))
+
+(deftest store-blob-with-id-and-tag
+  (test-store-blob-with-id-and-tag service))
+
+(deftest update-blob
+  (test-update-blob service))
+
+(deftest delete-blob
+  (test-delete-blob service))
+
