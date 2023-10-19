20e41f87c9d6c2e51bc359c60ea56654bbdb65e9
Juan Ramirez <jramirez.uy@gmail.com>
2023-10-19 18:34:49 -0300

    reset stream in blob->file, bump version 0.4.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.intception/blob-storage "0.4.3"
+(defproject com.intception/blob-storage "0.4.4"
   :description "Blob Storage Library"
   :url "https://github.com/intception/blob-storage"
   :license {:name "Eclipse Public License"
